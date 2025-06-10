4292145b6df148a1dee7d54bf33015809d9a4dbb
uni <uni@kali.Uni>
2025-06-10 21:15:30 +0100

    TO SAVE

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include "server/Server.hpp"
 
-int main(){
-    Server test(6667, "hajar");
+int main(int ac, char **av) {
+    if (ac != 3) {
+        std::cerr <<" <port> <password>" << std::endl;
+        return 1;
+    }
+    int port;
+    std::stringstream ss(av[1]);
+    ss >> port;
+    if (port < 1 || port > 65535) {
+        std::cerr << "Invalid port number." << std::endl;
+        return 1;
+    }
+    Server test(port, av[2]);
     test.parse_cmd(":nick!user@host JOIN #channel :Hello everyone!");
     test.server_socket();
 }
